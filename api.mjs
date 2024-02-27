9536c38446c41e5207d922e1ce62314c0de9b331
Jose Weiler <jgcarmoweiler@gmail.com>
2024-02-27 12:04:49 -0400

    Adicionando Cors e atualizando vetor de carros
    Co-authored-by: Alison Moura <alison.oghino@gmail.com>

--- api.mjs
+++ api.mjs
@@ -1,16 +1,43 @@
 // import http from "node:http"; // importação do módulo HTTP do nodejs
 import express from "express";
+import cors from "cors";
 
 const hostname = "127.0.0.1"; // valor do ip local (127.0.0.1 ou localhost)
 const port = 3000; // porta lógica de execução
 const carros = [
-  { modelo: "Uno", ano: "2023", cor: "vermelho", vendido: false, imagem: "" },
-  { modelo: "gol", ano: "2023", cor: "branco", vendido: false, imagem: "" },
-  { modelo: "ka", ano: "2023", cor: "preto", vendido: false, imagem: "" },
+  {
+    id: 1,
+    modelo: "Uno",
+    ano: "2023",
+    cor: "vermelho",
+    placa: "OON7895",
+    vendido: false,
+    imagem:
+      "https://cdn.motor1.com/images/mgl/2NO4RB/s1/visual-do-fiat-uno-ciao-traz-detalhes-na-lateral.webp",
+  },
+  {
+    id: 2,
+    modelo: "gol",
+    ano: "2023",
+    cor: "branco",
+    placa: "XXX2024",
+    vendido: false,
+    imagem: "",
+  },
+  {
+    id: 3,
+    modelo: "ka",
+    ano: "2023",
+    cor: "preto",
+    placa: "YYY2020",
+    vendido: false,
+    imagem: "",
+  },
 ];
 
 // Criação do servidor web
 const app = express();
+app.use(cors()); // habilitando CORS
 app.use(express.json()); //habilitando json no body das requisições
 
 app.get("/", function (req, res) {
